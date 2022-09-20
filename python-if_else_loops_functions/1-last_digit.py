786891ecd0033132229af0211b098aef2e2165f5
paosua86 <paolasuarezb1986@gmail.com>
2022-09-20 11:35:12 -0500

    changed print

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,11 +5,11 @@ if number > 0:
     numberLast = number % 10
 else:
     numberLast = number % -10
-print("Last digit of", end=" ")
+print("Last digit of", number, "is", numberLast, end=" ")
 if numberLast > 5:
-    print(f"{number} is {numberLast} and is grater than 5")
+    print("and is grater than 5")
 elif numberLast == 0:
-    print(f"{number} is {numberLast} and is 0")
+    print("and is 0")
 else:
-    print(f"{number} is {numberLast} and is less than 6 and not 0")
+    print("and is less than 6 and not 0")
 exit(0)
