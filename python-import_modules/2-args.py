3382cc675fe4cb6a4221117dc0a234cb4d31aec1
paosua86 <paolasuarezb1986@gmail.com>
2022-09-21 16:38:13 -0500

    changed 0 and 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     argv_len = len(argv)
-    print("{} arguments:".format(argv_len - 1))
+    if argv_len == 1:
+        print("{} arguments.".format(argv_len - 1))
+    elif argv_len == 2:
+        print("{} argument:".format(argv_len - 1))
+    else:
+        print("{} arguments:".format(argv_len - 1))
     for i in range(1, argv_len):
         print("{}: {}".format(i, argv[i]))
