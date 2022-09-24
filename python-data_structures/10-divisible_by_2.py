8611483bdeb55c6244474eba9745423ededa5034
paosua86 <paolasuarezb1986@gmail.com>
2022-09-23 19:00:55 -0500

    10 working

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = my_list.copy()
+    for element in new_list:
+        if (my_list[element] % 2) == 0:
+            new_list[element] = True
+        else:
+            new_list[element] = False
+    return (new_list)
