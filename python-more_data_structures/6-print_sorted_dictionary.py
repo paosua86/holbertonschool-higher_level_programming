4790647daa5f39d46a8a46f4df5fb69f816f0aaf
paosua86 <paolasuarezb1986@gmail.com>
2022-09-26 11:29:40 -0500

    6 working

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+from re import L
+
+
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}". format(key, a_dictionary[key]))
