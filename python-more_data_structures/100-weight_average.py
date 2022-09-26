efcd64f5703ebb175420536b7789d65075d65946
paosua86 <paolasuarezb1986@gmail.com>
2022-09-26 16:30:55 -0500

    style

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -2,7 +2,7 @@
 def weight_average(my_list=[]):
     if my_list:
         num = 0
-        dem = 0 
+        dem = 0
         for tup in my_list:
             num += (tup[0] * tup[1])
             dem += tup[1]
