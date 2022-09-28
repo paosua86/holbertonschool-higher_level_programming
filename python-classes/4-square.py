18fb308d8414ada50f20418204eeb431af12b678
paosua86 <paolasuarezb1986@gmail.com>
2022-09-28 15:18:07 -0500

    4 working

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return (self.__size) ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
