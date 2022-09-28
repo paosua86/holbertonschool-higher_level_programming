a7101a81fdcbe7aa067f18ba888fcc4274ffc014
paosua86 <paolasuarezb1986@gmail.com>
2022-09-28 11:00:47 -0500

    1 working

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    """Represents a square
+    Atributes:
+        __size (int): Private instance attribute.
+    """
+    def __init__(self, size):
+        self.__size = size
