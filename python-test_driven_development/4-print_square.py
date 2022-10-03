5b4a145d1de241d119bebbcea983b2b5122a6bd3
paosua86 <paolasuarezb1986@gmail.com>
2022-10-03 17:36:13 -0500

    4 working

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -8,12 +8,10 @@ The module contains one function, matrix_divided(matrix, div)
 
 def print_square(size):
     """Write a function that prints a square with the character #"""
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if type(size) is float and size < 0:
-        raise TypeError("size must be an integer")
+    if size < 0:
+        raise TypeError("size must be >= 0")
     for i in range(size):
         for j in range(size):
             print("#", end="")
