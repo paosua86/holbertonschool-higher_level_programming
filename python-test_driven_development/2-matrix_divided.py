71473abd335fc47161a458ea1d67a2d242123559
paosua86 <paolasuarezb1986@gmail.com>
2022-10-04 15:43:28 -0500

    fixed

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,16 +8,24 @@ The module contains one function, matrix_divided(matrix, div)
 
 def matrix_divided(matrix, div):
     """divides all elements of a matrix by div"""
-    if type(matrix) is not list:
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not matrix or not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     
     for row in matrix:
         if isinstance(row, list) and row:
             for col in row:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                if not isinstance(col, list) and col:
+                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        
     
-    if type(div) is not int and type(div) is not float:
+    
+    if not isinstance(div, float) and not isinstance(div, int):
         raise TypeError("div must be a number")
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-    return [[round(i / div, 2) for i in l] for l in matrix]
+    
+
+    matrix_1 = []
+    for x in matrix:    
+        matrix_1.append(list(map(lambda y: round(y / div, 2), x)))
+    return matrix_1
