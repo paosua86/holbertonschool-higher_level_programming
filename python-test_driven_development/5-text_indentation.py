7cab4877ffd23214ea99f82a5ebd28bc096bafd2
paosua86 <paolasuarezb1986@gmail.com>
2022-10-04 12:23:42 -0500

    6 working

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is the "5-text_indentation" module.
+
+The module contains one function, text_identation(text)
+"""
+
+
+def text_identation(text):
+    """"Write a function that prints a text with 2 new lines after each of these characters: ., ? and :"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    if a
+    
