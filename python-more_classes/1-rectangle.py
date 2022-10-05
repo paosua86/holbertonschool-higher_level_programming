a33e0be1e67c79ee6bdd325d4b134c374b05383a
paosua86 <paolasuarezb1986@gmail.com>
2022-10-05 10:36:38 -0500

    1 try

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -23,7 +23,7 @@ class Rectangle:
 
     @property
     def height(self):
-        return self.height
+        return self.__height
 
     @height.setter
     def height(self, value):
