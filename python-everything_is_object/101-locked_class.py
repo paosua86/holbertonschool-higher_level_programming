3000ac212f84499f989bd41e31f4c365aa1da052
paosua86 <paolasuarezb1986@gmail.com>
2022-10-10 12:17:59 -0500

    task

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""Write a class LockedClass"""
+
+
 class LockedClass:
-    """Write a class LockedClass with no class or object attribute,
-    that prevents the user from dynamically creating new
+    """prevents the user from dynamically creating new
     instance attributes, except if the new instance attribute is
     called first_name"""
     __slot__ = ["fist_name"]
