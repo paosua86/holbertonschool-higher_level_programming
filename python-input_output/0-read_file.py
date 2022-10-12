28baeff2ce7284e6352b43984f0a96f6ea426402
paosua86 <paolasuarezb1986@gmail.com>
2022-10-12 10:13:16 -0500

    0 working

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains read_file function
+"""
+
+
+def read_file(filename=""):
+    """function that reads a text file
+    (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
