4dddcf42184d2560bc479ed25440d84e61ba0b2c
paosua86 <paolasuarezb1986@gmail.com>
2022-10-12 16:09:44 -0500

    12 working

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 """
-contains the read-file function
+contains class pascal_triangle
 """
 
 
-def read_file(filename=""):
-    """function that reads a text file (UTF8) and prints it to stdout:"""
-    with open(filename, "r", encoding='utf-8') as f:
-        print(f.read(), end="")
+def pascal_triangle(n):
+    """returns a list of lists of integers representing
+    the Pascal’s triangle of n:"""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [[1]]
+    list = [[1]]
+    while len(list) != n:
+        last_n = list[-1]
+        new_n = [1]
+        for i in range(len(last_n) - 1):
+            new_n.append(last_n[i] + last_n[i + 1])
+        new_n += [1]
+        list.append(new_n)
+    return list
