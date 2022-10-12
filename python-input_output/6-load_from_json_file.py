68781800cb82faab4fd66fcd0c47bd40a2bbe941
paosua86 <paolasuarezb1986@gmail.com>
2022-10-12 11:37:18 -0500

    6 working

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+contains load_from_json_file fuction
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”"""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
