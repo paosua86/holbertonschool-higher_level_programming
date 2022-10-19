92edb20a6fdc45283cdb04098ba14afa6fb378ce
paosua86 <paolasuarezb1986@gmail.com>
2022-10-19 12:11:39 -0500

    json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -29,9 +29,15 @@ class Base:
     @staticmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation"""
-        filename = cls.__name__+".json"
+        filename = cls.__name__ + ".json"
         if list_objs is not None:
-            for i in list_objs:
-                list_objs = [i.to_dictionary() for i in list_objs]
+            list_objs = [i.to_dictionary() for i in list_objs]
         with open(filename, "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Update the class Base"""
+        if json_string is None or not json_string:
+            return "[]"
+        return json.loads(json_string)
