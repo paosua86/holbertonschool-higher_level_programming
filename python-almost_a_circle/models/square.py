0c6ef46b2d50e27f3aea6465e5e60a7b27b0199e
paosua86 <paolasuarezb1986@gmail.com>
2022-10-19 10:20:01 -0500

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""
+Includes class Square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """representing square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """inherits from Rectangle"""
+        super().__init__(size, size, x, y, id)
+
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set width"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """string representation"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width,)
+
+    def update(self, *args, **kwargs):
+        """update print in stdout"""
+        if len(args):
+            for i, a in enumerate(args):
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.width = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "width" in kwargs:
+                self.width = kwargs["width"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        """returns the dictionary representation"""
+        return {"x": self.x, "y": self.y, "id": self.id,
+                "size": self.width}
+
