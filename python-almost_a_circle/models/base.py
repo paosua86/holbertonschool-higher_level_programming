00e50ba82c4a23b8dc105944c382d09e697d9f44
paosua86 <paolasuarezb1986@gmail.com>
2022-10-19 11:32:13 -0500

    json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,6 +4,9 @@ Contains the "Base" class
 """
 
 
+import json
+
+
 class Base:
     """Bse class"""
     __nb_objects = 0
@@ -15,3 +18,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictorionaries):
+        """ returns the JSON string representation"""
+        if list_dictorionaries is None:
+            return "[]"
+        return json.dumps(list_dictorionaries)
+
+    @staticmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation"""
+        filename = cls.__name__ + ".json"
+        list = []
+        if list_objs is None:
+            return "[]"
+        for i in list_objs:
+            list.append(cls.to_dictionary(i))
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(list))
+
+
