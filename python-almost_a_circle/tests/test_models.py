582ac17706090feaa5ec76ded31c9bab4f54b0ef
paosua86 <paolasuarezb1986@gmail.com>
2022-10-21 11:25:39 -0500

    test

--- python-almost_a_circle/tests/test_models.py
+++ python-almost_a_circle/tests/test_models.py
@@ -5,13 +5,9 @@ from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
-class Base_Test(unittest.TestCase):
-    """test all"""
-
-
-    def test(self):
-        """test"""
-        Base()
+class Test_base(unittest.TestCase):
+    def test_alone(self):
+        self.assertEqual(Base().id, 1)
 
     def test0(self):
         """type error"""
