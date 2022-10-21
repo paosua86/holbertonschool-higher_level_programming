8a5379f19391b9b790befd733767ff7f849d9a1c
paosua86 <paolasuarezb1986@gmail.com>
2022-10-21 14:52:35 -0500

    tests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,6 +8,31 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     '''contains all test of class Rectangle'''
 
+    def test_rectangle_creation_1(self):
+        rect = Rectangle(1, 2)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+
+    def test_rectangle_creation_2(self):
+        rect = Rectangle(1, 2, 3)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+
+    def test_rectangle_creation_3(self):
+        rect = Rectangle(1, 2, 3, 4)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
+    def test_rectangle_creation_3(self):
+        rect = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(rect.width, 1)
+        self.assertEqual(rect.height, 2)
+        self.assertEqual(rect.x, 3)
+        self.assertEqual(rect.y, 4)
+
     def test_type(self):
         self.assertRaises(TypeError, Rectangle, width = '2')
         self.assertRaises(TypeError, Rectangle, width = float('NaN'))
