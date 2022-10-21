46b62c31260bd715b5879ec1931796bd22cfeae9
paosua86 <paolasuarezb1986@gmail.com>
2022-10-21 12:35:48 -0500

    tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -20,3 +20,21 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base2.id, 2)
         base3 = Base(125)
         self.assertEqual(base3.id, 125)
+
+    def test_to_json_string_none(self):
+        self.assertEqual(Base.to_json_string(None), '[]')
+
+    def test_to_json_string_empty(self):
+        self.assertEqual(Base.to_json_string([]), '[]')
+
+    def test_to_json_string_exists(self):
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string_none(self):
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
+
+    def test_from_json_string_exists(self):
+        self.assertEqual(Base.from_json_string('[{ "id": 89 }]'), [{'id': 89}])
