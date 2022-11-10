d7aeedb514c1a4e50446538747ee86865f0bfac2
paosua86 <paolasuarezb1986@gmail.com>
2022-11-10 13:22:11 -0500

    102 working

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,3 @@
+-- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month >= 7 AND month < 9 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+
