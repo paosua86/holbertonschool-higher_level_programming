9844f3d00d03a55a74d43cbe402f8f6b946535ee
paosua86 <paolasuarezb1986@gmail.com>
2022-11-15 15:23:10 -0500

    0 style

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Write a script that lists all states from the database hbtn_0e_0_usa:
-
-Your script should take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
+Your script should take 3 arguments:
+mysql username, mysql password and database name
+(no argument validation needed)
 You must use the module MySQLdb (import MySQLdb)
 Your script should connect to a MySQL server running on localhost at port 3306
 Results must be sorted in ascending order by states.id
@@ -9,11 +10,14 @@ Results must be displayed as they are in the example below
 Your code should not be executed when imported
 """
 
-import MySQLdb
+
 from sys import argv
 
+import MySQLdb
+
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     # Print results in comma delimited format
