4a8ee33bf48d269cb21d4400829d5e1ad6f893a1
paosua86 <paolasuarezb1986@gmail.com>
2022-12-02 17:40:19 -0500

    7 working

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,6 +3,9 @@
 
 const myArray = 'C is fun';
 const j = process.argv[2];
+if (isNaN(j)) {
+  console.log('Missing number of occurrences');
+}
 for (let i = 0; i < j; i++) {
   console.log(myArray);
 }
