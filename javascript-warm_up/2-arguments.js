3aefa73d47c1ae806cb2488a7a60e3ccea1008fa
paosua86 <paolasuarezb1986@gmail.com>
2022-12-02 16:58:07 -0500

    2 working

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Prints a message depending of the number of arguments passed
+
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else {
+  console.log('Arguments found');
+}
