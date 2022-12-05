d931760b2377cc78a6885775c76d3c3f36072452
paosua86 <paolasuarezb1986@gmail.com>
2022-12-05 18:11:01 -0500

    12

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
-// Write a script that searches the second biggest integer in the list of arguments.
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89;
+console.log(myObject);
