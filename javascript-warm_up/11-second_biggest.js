3a03cbd249a1c3adb6192f4a4a473fde9235f9c0
paosua86 <paolasuarezb1986@gmail.com>
2022-12-05 18:01:55 -0500

    11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 // Write a script that searches the second biggest integer in the list of arguments.
 
 const array = process.argv.slice(1, process.argv.length);
-if (array.length < 1) {
-    console.log(0);
+if (array.length < 3) {
+    console.log('0');
   } else {
     const list_string = array.map(Number);
     const list = list_string.sort(function(a,b){return b-a})[2];
