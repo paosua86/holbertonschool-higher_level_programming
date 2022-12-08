ba089eb60d866478f5a23c45ab7edb6dad9d399b
paosua86 <paolasuarezb1986@gmail.com>
2022-12-08 18:45:24 -0500

    test

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Write a script that prints the title of a Star Wars
+// movie where the episode number matches a given integer
+const request = require('request');
+const { argv } = require('process');
+const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const obtained = JSON.parse(body);
+    console.log(`${obtained.title}`);
+  }
+});
