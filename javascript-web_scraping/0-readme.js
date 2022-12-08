a84822079fe0d82c964d8f9c20e24fa1c2f85171
paosua86 <paolasuarezb1986@gmail.com>
2022-12-08 15:26:35 -0500

    0 working

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that reads and prints the content of a file
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
