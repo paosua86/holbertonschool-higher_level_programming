3c8a0706607c4168902554ab55017d60596ba4db
paosua86 <paolasuarezb1986@gmail.com>
2022-12-08 17:44:48 -0500

    3 working

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,14 @@
 // Write a script that prints the title of a Star Wars
 // movie where the episode number matches a given integer
 const request = require('request');
-const arg2 = process.argv[2];
-const url = 'http://swapi.co/api/films/' + arg2;
+const { argv } = require('process');
+const url = `https://swapi-api.hbtn.io/api/films/${argv[2]}`
+
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(response.body).title);
+    const obtained = JSON.parse(body);
+    console.log(`${obtained.title}`);
   }
 });
